b50fc4b2d5e6c5559f21b6688a1a8d1fc2cd7001
Aakriti Mehta <aakritimehta15@gmail.com>
2024-04-25 23:39:32 +0530

    aoa

--- travellingsalesman.cpp
+++ travellingsalesman.cpp
@@ -32,7 +32,7 @@ int main() {
     cin >> n;
 
     vector<vector<int>> graph(n, vector<int>(n, 0));
-    cout << "Enter the distances between cities: " << endl;
+    cout << "Enter the distances between cities(matrix form): " << endl;
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < n; ++j) {
             cin >> graph[i][j];
